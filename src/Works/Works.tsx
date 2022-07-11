9d84a19375ee3818c2ae115e7d2825197b147e72
1t-n00b <mummintrol@gmail.com>
2022-07-11 19:44:10 +0300

    add Title component

--- src/Works/Works.tsx
+++ src/Works/Works.tsx
@@ -1,21 +1,21 @@
-import React from "react";
-import style from "./Works.module.css"
-import styleContainer from "../../src/common/styles/Container.module.css"
-import Work from "./Work/Work";
+import React from 'react';
+import style from './Works.module.css';
+import styleContainer from '../../src/common/styles/Container.module.css';
+import Work from './Work/Work';
+import Title from '../common/components/Title';
 
 const Works = () => {
     return (
         <div className={style.worksBlock}>
             <div className={`${styleContainer.container} ${style.worksContainer}`}>
-                <h3 className={style.title}>My Works</h3>
+                <Title title={"My Projects"}/>
                 <div className={style.works}>
-                    <Work title={"Projects Name"}
-                          text={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."}/>
-                    <Work title={"Projects Name"}
-                          text={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."}/>
-                    <Work title={"Projects Name"}
-                          text={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."}/>
-
+                    <Work title={'Projects Name'}
+                          text={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'}/>
+                    <Work title={'Projects Name'}
+                          text={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'}/>
+                    <Work title={'Projects Name'}
+                          text={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'}/>
                 </div>
             </div>
         </div>
---
+++ src/common/components/Title.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import style from './Title.module.css';
+
+type TitlePropsType = {
+    title: string
+}
+
+const Title: React.FC<TitlePropsType> = (props: TitlePropsType) => {
+    return (
+        <div className={style.title}>
+            <h3>{props.title}</h3>
+        </div>
+    );
+};
+
+export default Title;
