80cbfebe259d5107c3430aa13c32624279e94945
1t-n00b <mummintrol@gmail.com>
2022-08-15 15:46:14 +0300

    ref

--- src/Header/BurgerMenu/BurgerMenu.tsx
+++ src/Header/BurgerMenu/BurgerMenu.tsx
@@ -2,15 +2,22 @@ import React, {useState} from 'react';
 import Nav from '../nav/Nav';
 import {Burger} from '@mantine/core';
 import burger from './BurgerMenu.module.scss';
+import {Fade} from 'react-awesome-reveal';
 
 const BurgerMenu: React.FC = () => {
     const [opened, setOpened] = useState(false);
     return (
         <div className={burger.burgerMenu}>
             {opened ?
-                <Nav/>
+                <Fade direction={'left'}>
+                    <Nav/>
+                </Fade>
+
                 :
-                <a href="https://github.com/1t-n00b" target="_blank" rel="noreferrer">it.n00b.pel</a>
+                <Fade direction={'right'}>
+                    <a href="https://github.com/1t-n00b" target="_blank" rel="noreferrer">it.n00b.pel</a>
+                </Fade>
+
             }
             <Burger
                 opened={opened}
