346c305462899ad9dae63e42930a2436e5ad3420
1t-n00b <mummintrol@gmail.com>
2022-08-16 13:34:02 +0300

    connecting form

--- src/Contact/Contact.tsx
+++ src/Contact/Contact.tsx
@@ -1,18 +1,32 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import style from './Contact.module.scss';
 import Title from '../common/components/Title';
+import emails from '@emailjs/browser';
 
 const Contact = () => {
+    const form = useRef('');
+    const sendEmail = (e: any) => {
+        e.preventDefault();
+        emails.sendForm('service_oslaeln', 'template_cchcvcm', form.current, 'zyj2Twca9QtlKiKkL')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+    };
+
     return (
+
         <div className={style.contactBlock} id="contact">
-            <form action="" className={style.formBlock}>
+            <form action="" className={style.formBlock}
+                // @ts-ignore
+                  ref={form} onSubmit={sendEmail}>
                 <Title title={'Contact'}/>
-                <input type="tel" placeholder={'Phone'}/>
-                <input type="email" placeholder={'Email'}/>
-                <textarea name="" id="" placeholder={'Comments'}></textarea>
-                <button>SEND MESSAGE</button>
+                <input type="tel" name="phone" placeholder={'Phone'}/>
+                <input type="email" name="email" placeholder={'Email'}/>
+                <textarea name="message" id="" placeholder={'Comments'}></textarea>
+                <button type={'submit'}>SEND MESSAGE</button>
             </form>
-            {/*</div>*/}
         </div>
     );
 };
